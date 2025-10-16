a9eda270192502529b8b85172f7bb1639864237b
Ahil <ahilmakwana26@gmail.com>
2025-10-16 23:20:13 +0530

    this is first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index(){
+        echo "Hello Ahil Now use have start making for ERP system";
+    }
+}
