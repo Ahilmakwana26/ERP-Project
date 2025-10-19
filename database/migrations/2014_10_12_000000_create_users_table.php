78ba33bf2566814260761b23e53d1349645c8449
Ahil <ahilmakwana26@gmail.com>
2025-10-19 22:37:15 +0530

    createtablr user,employee.companys, anf role

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,17 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
+            $table->increments('id')->unsigned();
             $table->string('role');
             $table->integer('company_id');
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('email_verified_at');
+            $table->longText('user_permission')->nullable();
+            $table->enum('status',['Active','Inactive']);
+            $table->string('email_verified_at')->nullable();
+            $table->enum('authorize_type',['otp','key','both','none'])->default('none');
+            $table->longText('unrestricted_permission')->nullable();
             $table->rememberToken();
             $table->softDeletes();
             $table->timestamps();
---
+++ database/migrations/2025_10_19_161825_create_employees_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->integer('company_id');
+            $table->integer('user_id');
+            $table->integer('e_id');
+            $table->string('name');
+            $table->string('email');
+            $table->longText('present_type');
+            $table->integer('casual_leave');            
+            $table->integer('leave_penalty');
+            $table->tinyInteger('status')->comment('1=Trainee , 2=Onroll ,3=Relieved');
+            $table->integer('allow_overtime');
+            $table->integer('flexible_breack_allow');
+            $table->integer('miniman_overtime_require');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
